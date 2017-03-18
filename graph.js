b6b2a7214e4e26828fc2a7a19b0bc5a630209d1d
Jonathan <Enirrisky@gmail.com>
2017-03-18 15:07:49 -0700

    pseudo graph code, style changes

---
+++ graph.js
@@ -0,0 +1,24 @@
+/*GRAPH ALGORIGTHM*/
+
+//get data
+
+//if user has data for more than one month stored, start graph
+
+//find out how many months have data
+
+//divide canvas width by number of months that have data
+
+//find max value for income, expense, or balance between all months to set range
+
+//draw text to label months of graph along width dividers
+
+//draw range along left side of canvas
+
+//start income line at first month income value
+
+//draw line to next month income value
+
+//continue until end month
+
+//continue drawing lines for balance and expenses
+
--- script.js
+++ script.js
@@ -5,7 +5,6 @@ window.onload = function(){
 	/*
 		Toggle section display
 	*/
-	console.log(window.innerHeight);
 	document.getElementById('mainApp').style.maxHeight = (window.innerHeight - 50);
 	
 	var sections = document.getElementsByClassName('section-title');
@@ -15,16 +14,26 @@ window.onload = function(){
 	
 	for (s = 0; s < sections.length; s++){
 		sections[s].onclick = function(e){
+			getTotals(document.getElementsByClassName('budgetInput'));
 			var inputsDiv = this.parentElement.children[1];
+			var arrow = this.children[0];
 			if(inputsDiv.style.display === 'block' || inputsDiv.style.display === ''){
 				inputsDiv.style.display = 'none';
 				
 				//flip arrow vertical
+				arrow.style['-webkit-transform'] =  'rotate(90deg) scaleY(-1)';
+				arrow.style['-moz-transform'] =  'rotate(90deg) scaleY(-1)';
+				arrow.style['-o-transform'] = 'rotate(90deg) scaleY(-1)';
+				arrow.style['transform'] = 'rotate(90deg) scaleY(-1)';
 			}
 			else{
 				inputsDiv.style.display = 'block';;
 				
 				//flip arrow vertical
+				arrow.style['-webkit-transform'] =  'rotate(270deg) scaleY(1)';
+				arrow.style['-moz-transform'] =  'rotate(270deg) scaleY(1)';
+				arrow.style['-o-transform'] = 'rotate(270deg) scaleY(1)';
+				arrow.style['transform'] = 'rotate(270deg) scaleY(1)';
 			}
 		};
 	};
@@ -33,11 +42,9 @@ window.onload = function(){
 		On input change
 	*/
 	var inputs = document.getElementsByClassName('budgetInput');
-	console.log(inputs);
 	for(i = 0; i < inputs.length; i++){
-		inputs[i].onchange = function(e){
-			var totals = getTotals(document.getElementsByClassName('budgetInput'));
-			console.log(totals);
+		inputs[i].onkeyup = function(e){
+			getTotals(document.getElementsByClassName('budgetInput'));
 		}
 	}
 	function getTotals(inputsArr){
@@ -48,17 +55,20 @@ window.onload = function(){
 		};
 		
 		for(l = 0; l < inputsArr.length; l++){
-			var type = inputs[l].getAttribute('data-type');
-			var num = parseFloat(inputs[l].value);
-			if(isNaN(num)) num = 0;
-			
-			if(type === 'expense'){
-				totals.balance -= num;
-				totals.expense += num;
-			}
-			else if(type === 'income'){
-				totals.balance += num;
-				totals.income += num;
+			var display = inputs[l].parentElement.parentElement.style.display;
+				if(display === 'block' || display === ''){
+				var type = inputs[l].getAttribute('data-type');
+				var num = parseFloat(inputs[l].value);
+				if(isNaN(num)) num = 0;
+				
+				if(type === 'expense'){
+					totals.balance -= num;
+					totals.expense += num;
+				}
+				else if(type === 'income'){
+					totals.balance += num;
+					totals.income += num;
+				}
 			}
 		}
 		
@@ -70,10 +80,19 @@ window.onload = function(){
 			balanceTotal.style.color = 'red';
 		}
 		else{
-			balanceTotal.style.color = 'black';
+			balanceTotal.style.color = 'white';
 		}
-		return totals;
+		//console.log(totals);
 	}
+	
+	/*
+		On month change
+	*/
+	document.getElementById('month').addEventListener('change', function(){
+		console.log(this.innerHTML);
+		document.getElementById('monthDisplay').innerHTML = this.value+' budget';
+	});
+	
 	
 	
 };
